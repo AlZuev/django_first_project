467799c30c9cc8f4bfd31449454ee1f7cf337c95
AlZuev <ya.al.zuev@yandex.ru>
2019-02-01 23:28:14 +0300

    First version

---
+++ static/js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $("#btn-service1").click(function(){
+        $('#service-title1').text("Верстка сайта");
+        $('#service-text1').text("Создание статического сайта по готовому шаблону");
+        $('#service-text2').text("Разработка уникального дизайна и создание неповторимого сайта");
+        $('#service-text3').text("Создание структуры сайта с помощью различных CMS");
+    });
+});
+
+$(document).ready(function(){
+    $("#btn-service2").click(function(){
+        $('#service-title1').text("Сайт под ключ");
+        $('#service-text1').text("Создание динамического сайта любого уровня сложности");;
+        $('#service-text2').text("Обеспечение хостинга");
+        $('#service-text3').text("Продолжительная поддержка сайта");
+    });
+});
+
+$(document).ready(function(){
+    $("#btn-service3").click(function(){
+        $('#service-title1').text("Оптимизация сайта");
+        $('#service-text1').text("Рефакторинг сайт на новую платформу");
+        $('#service-text2').text("Поиск ошибок в структуре и динамических эффектах сайта");
+        $('#service-text3').text("Увеличение скорости работы сайта за счет оптимизации HTML и СSS элементов");
+    });
+});
+
+$(document).ready(function(){
+    $("#btn-service4").click(function(){
+        $('#service-title1').text("Работа с базой данных");
+        $('#service-text1').text("Разработка сайтов высокой сложности");
+        $('#service-text2').text("Создание базы данных");
+        $('#service-text3').text("Поддержка и дополнение имеющейся базы данных");
+    });
+});
+
+$(document).ready(function(){
+    $("#btn-service5").click(function(){
+        $('#service-title1').text("Текст");
+        $('#service-text1').text("Текст");
+        $('#service-text2').text("Текст");
+        $('#service-text3').text("Текст");
+    });
+});
